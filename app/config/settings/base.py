2025dee0e54667ab05c3688a8448bd8135fa80a9
biobr131 <takayuki.yamada.19@gmail.com>
2025-02-08 10:19:49 +0900

    added drf-spectacular-sidecar

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "django_filters",
     "drf_spectacular",
+    "drf_spectacular_sidecar",
     "import_export",
     "silk",
     "strawberry_django", 
@@ -127,4 +128,7 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "Your project description",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "REDOC_DIST": "SIDECAR",
 }
