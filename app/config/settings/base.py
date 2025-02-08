b0d39f20f3ad1a008ece28490c2b8e93964a019a
biobr131 <takayuki.yamada.19@gmail.com>
2025-02-08 15:04:50 +0900

    added settings for drf_spectacular

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-RESTFRAMEWORK = {
+REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
 }
 
--- app/config/urls/prod.py
+++ app/config/urls/prod.py
@@ -1,10 +1,12 @@
-from django.conf.urls.static import static
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path("", include("accounts.urls.prod")),
     path("admin/", admin.site.urls),
     path("silk/", include("silk.urls", namespace="silk")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
